43c5ef600e28c7b096029eb5d126c767277f206e
samaritanodev <lesly.samaritano.ayala@gmail.com>
2022-07-28 01:12:23 -0500

    tag add bottom left for card products

--- appsaveproducts/lib/widgets/product_card.dart
+++ appsaveproducts/lib/widgets/product_card.dart
@@ -14,8 +14,10 @@ class ProductCard extends StatelessWidget {
         height: 400,
         decoration: _cardBorders(),
         child: Stack(
+          alignment: Alignment.bottomLeft,
           children: const [
             _BackgroundImage(),
+            _ProductDetails(),
           ],
         ),
       ),
@@ -35,19 +37,73 @@ class ProductCard extends StatelessWidget {
       );
 }
 
+class _ProductDetails extends StatelessWidget {
+  const _ProductDetails({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const Color colorrosapalo = Color(0xFFF3E6D5);
+
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        decoration: _buildBoxDecoration(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Name product',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                color: colorrosapalo,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'id for product',
+              style: TextStyle(
+                color: colorrosapalo,
+                fontSize: 15,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => const BoxDecoration(
+        color: Color(0xFF5A3761),
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(25), topRight: Radius.circular(25)),
+      );
+}
+
 class _BackgroundImage extends StatelessWidget {
+  final Color colorrosapalo = const Color(0xFFF3E6D5);
   const _BackgroundImage({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      width: double.infinity,
-      height: 400,
-      child: FadeInImage(
-        placeholder: AssetImage('assets/load.gif'),
-        image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: const SizedBox(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage('assets/load.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
